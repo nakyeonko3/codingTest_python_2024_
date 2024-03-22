b6d97871c3ce2e9a9544248262cb7e43b1cfa350
nakyeonko3 <badaclock@gmail.com>
2024-03-22 22:59:18 +0900

    Changes

--- code/기타레슨.py
+++ code/기타레슨.py
@@ -1,12 +1,37 @@
 import sys
+
 input = sys.stdin.readline
-n = int(input())
-arr = [int(num) for num in input().split()]
+[n, m] = [int(b) for b in input().split()]
+lectures = [int(a) for a in input().split()]
 
-dp = [1] * n
+start = max(lectures)  # 최소 블루레이 크기의 하한
+end = sum(lectures)  # 최소 블루레이 크기의 상한
 
-print(dp)
-# for i in range(1, n):
-#     for j in range(i):
-#         if arr[i] > arr[j]:
-#             dp[i] = max(dp[i], dp[j] + 1)
+
+def get_cnt(size: int, lectures: list):
+    cnt = 1  # 블루레이 개수
+    total = 0  # 현재 블루레이에 녹화된 강의 길이 합
+    for lecture in lectures:
+        if total + lecture > size:
+            cnt += 1
+            total = lecture
+        else:
+            total += lecture
+    return cnt
+
+
+def get_min_blurary_size(start: int, end: int, lectures: list):
+    while start <= end:
+        mid = (start + end) // 2
+        if get_cnt(mid, lectures) <= m:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return start
+
+
+result = get_min_blurary_size(start, end, lectures)
+print(result)
+
+# if __name__ == '__main__':
+#     get_min_blurary_size(start, end, lectures)
